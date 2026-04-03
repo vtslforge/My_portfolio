5f17995db52a48c08707782bdde676687f2c1eb6
Vatsalya Singh <singhvatsalya16@gmail.com>
2026-04-03 15:46:01 +0530

    fixed loading issues

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -4,23 +4,20 @@ import TechStack from './_components/TechStack';
 import GithubStat from './_components/GithubStat';
 import Projects from './_components/Projects';
 import Contact from './_components/Contact';
-import { RouteReady, SplashProvider } from '@/components/splash-provider';
 
 const page = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   return (
-    <SplashProvider>
-      <main className="flex flex-col items-center">
-        <RouteReady />
-        <Intro />
-        <section className="flex flex-col w-[95vw] space-y-15 md:w-[75vw] mt-12">
-          <AboutSection />
-          <TechStack />
-          <GithubStat />
-          <Projects />
-          <Contact />
-        </section>
-      </main>
-    </SplashProvider>
+    <main className="flex flex-col items-center">
+      <Intro />
+      <section className="flex flex-col w-[95vw] space-y-15 md:w-[75vw] mt-12">
+        <AboutSection />
+        <TechStack />
+        <GithubStat />
+        <Projects />
+        <Contact />
+      </section>
+    </main>
   );
 };
 
